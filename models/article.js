9c401f3b122a695ec86599948c8d4813325537a8
abdelmajid-ramli <abdalmajidardesings@gmail.com>
2021-06-11 14:06:50 +0100

    test

--- models/article.js
+++ models/article.js
@@ -4,13 +4,11 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Article extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
+      Article.belongsTo(models.User)
+      Article.hasMany(models.Comment)
+      Article.belongsToMany(models.Tag, {through: 'ArticleTags'})
     }
   };
   Article.init({
